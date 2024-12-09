c31680985000cc4c9c476e800f69423737eb2013
xiaodo168 <xijin858@gmail.com>
2024-12-09 08:10:57 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
-const dockerHub = "https://registry-1.docker.io";
+const dockerHub = "https://registry-1.hary.us.kg";
 
 const routes = {
   // production
